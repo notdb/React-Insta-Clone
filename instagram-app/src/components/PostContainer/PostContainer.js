4efea354ff3bb10d9dde5864fcb71f47e0d1da48
Doug <me@douglasbowen.com>
2019-04-30 15:43:38 -0400

    change postcontainer from functional component to class component

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -3,36 +3,42 @@ import "./PostContainer.css";
 import CommentSection from "../CommentSection/CommentSection.js";
 import moment from "moment";
 
-function PostContainer(props) {
-  let today = props.dummyObj.timestamp;
-  return (
-    <div className="post-container">
-      <header className="header">
-        <img className="thumb-img" src={props.dummyObj.thumbnailUrl} />
-        <p>{props.dummyObj.username}</p>
-      </header>
-      <img className="post-img" src={props.dummyObj.imageUrl} />
-      <div className="icons">
-        <div className="row">
-          <p>Icon</p>
-          <p> Icon</p>
+class PostContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    let today = this.props.dummyObj.timestamp;
+    return (
+      <div className="post-container">
+        <header className="header">
+          <img className="thumb-img" src={this.props.dummyObj.thumbnailUrl} />
+          <p>{this.props.dummyObj.username}</p>
+        </header>
+        <img className="post-img" src={this.props.dummyObj.imageUrl} />
+        <div className="icons">
+          <div className="row">
+            <p>Icon</p>
+            <p> Icon</p>
+          </div>
+          <p>373 likes</p>
         </div>
-        <p>373 likes</p>
+        <div className="comment-thing">
+          {this.props.dummyObj.comments.map(comment => (
+            <CommentSection key={comment.id} comment={comment} />
+          ))}
+        </div>
+        <div className="comment-form">
+          <p>Input form goes here</p>
+          <p>...</p>
+          {moment()
+            .startOf(today)
+            .fromNow()}
+        </div>
       </div>
-      <div className="comment-thing">
-        {props.dummyObj.comments.map(comment => (
-          <CommentSection key={comment.id} comment={comment} />
-        ))}
-      </div>
-      <div className="comment-form">
-        <p>Input form goes here</p>
-        <p>...</p>
-        {moment()
-          .startOf(today)
-          .fromNow()}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default PostContainer;
